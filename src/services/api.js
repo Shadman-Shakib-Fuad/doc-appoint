08cbd20798aad33bac427a29f7dc192ffb03e7d1
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-21 23:12:04 +0600

    connect frontend with deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:
+    "https://doc-appoint-server-vavu.onrender.com",
 });
 
 api.interceptors.request.use(
