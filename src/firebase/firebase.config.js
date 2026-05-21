5c65d7a795c37c4bcaaf2be973c6927dc58f24bd
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-21 22:41:58 +0600

    setup firebase authentication

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAR8-D8xhXn-FyPqPBDi3-94uliF7WVw4U",
+  authDomain: "doc-appoint-7663c.firebaseapp.com",
+  projectId: "doc-appoint-7663c",
+  storageBucket: "doc-appoint-7663c.firebasestorage.app",
+  messagingSenderId: "564266801979",
+  appId: "1:564266801979:web:c0831c369e36b4d101b51e",
+  measurementId: "G-69EJ4MJK0C",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
