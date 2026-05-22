d4b0d6eb92db752bd72fd229f17afc1687ee80ed
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-22 08:19:34 +0600

    add custom 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,24 +1,22 @@
 import Link from "next/link";
 
-export default function NotFoundPage() {
+export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center px-5">
-      <div className="text-center">
-        <h1 className="text-8xl font-bold">
-          404
-        </h1>
+    <div className="min-h-screen flex flex-col items-center justify-center text-center px-5">
+      <h1 className="text-8xl font-black text-primary">
+        404
+      </h1>
 
-        <p className="text-2xl mt-5">
-          Page Not Found
-        </p>
+      <p className="mt-6 text-xl text-gray-500">
+        Page Not Found
+      </p>
 
-        <Link
-          href="/"
-          className="btn btn-primary mt-8"
-        >
-          Back To Home
-        </Link>
-      </div>
+      <Link
+        href="/"
+        className="btn btn-primary rounded-full mt-10"
+      >
+        Back Home
+      </Link>
     </div>
   );
 }
