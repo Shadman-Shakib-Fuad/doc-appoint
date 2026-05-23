94ae3d0b8546bf290fe2556c3d1c5fb7d48f1f8b
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-23 18:13:05 +0600

    Prepared project for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:5000",
+    "https://doc-appoint-server1.onrender.com",
 });
 
 api.interceptors.request.use(
